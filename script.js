702d47a8d8f1c2f067e2153dae1bde5aad682674
Eric Chan <ericchan024@gmail.com>
2023-08-03 14:44:17 -0400

    added function for minimax

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ const O = Player('o')
 
 function gameController(e) {
     const cell = e.target
+    if (cell.classList.contains('o')) {
+        cell.stopPropagation()
+    }
     gameModule.placeMark(cell)
     gameModule.checkWin()
     gameModule.checkDraw()
@@ -37,13 +40,79 @@ let gameModule = (function gameModule() {
             ['', '', ''],
         ]
         _oTurn = false
-        _currentTurn = X.mark
+        _currentTurn = X
         board.classList.add(X.mark)
         cellElements.forEach((cell) => {
             cell.addEventListener('click', gameController, { once: true })
             cell.classList.remove(X.mark)
             cell.classList.remove(O.mark)
         })
+    }
+
+    function minimax(newBoard, player) {
+        const availSpots = getValidMoves()
+
+        if (checkWin() && player === X) {
+            return { score: -10 }
+        }
+        if (checkWin() && player === O) {
+            return { score: 10 }
+        }
+        if (availSpots.length === 0) {
+            return { score: 0 }
+        }
+
+        const moves = []
+        // loops through available spots
+        for (let i = 0; i < availSpots.length; i += 1) {
+            // creating an object for each spot and storing position of empty spot
+            const move = {}
+            const { row } = availSpots[i]
+            const { col } = availSpots[i]
+            move.index = [row, col]
+
+            // sets empty spot to the current player
+            // eslint-disable-next-line no-param-reassign
+            newBoard[row][col] = player.mark
+
+            // collects the score from calling minimax on the opponent of current player
+            if (player === O) {
+                const result = minimax(newBoard, X)
+                move.score = result.score
+            } else {
+                const result = minimax(newBoard, O)
+                move.score = result.score
+            }
+
+            // resets the spot to empty
+            newBoard[row][col] = ''
+
+            // push the object into the array
+            moves.push(move)
+        }
+
+        // evalutes the best move in the moves array
+        let bestMove
+        // if AI turn, it will loop over moves to search for one with higest score (e.g. most optimum move for ai)
+        if (player === O) {
+            let bestScore = -1000
+            for (let i = 0; i < moves.length; i += 1) {
+                if (moves[i].score > bestScore) {
+                    bestScore = moves[i].score
+                    bestMove = i
+                }
+            }
+        } else {
+            // else loop over move with lowest score (aka most optimum move for human and least optimum for AI)
+            let bestScore = 1000
+            for (let i = 0; i < moves.length; i += 1) {
+                if (moves[i].score < bestScore) {
+                    bestScore = moves[i].score
+                    bestMove = i
+                }
+            }
+        }
+        return moves[bestMove]
     }
 
     function placeMark(cell) {
@@ -77,11 +146,19 @@ let gameModule = (function gameModule() {
     function aiMove() {
         const randomIndex = Math.floor(Math.random() * getValidMoves().length)
 
+        // const bestMove = minimax(_gameboard, O)
+        // const row = bestMove.index[0]
+        // const col = bestMove.index[1]
+        // console.log(bestMove)
+
         const { row } = getValidMoves()[randomIndex]
         const { col } = getValidMoves()[randomIndex]
         const cell = document.querySelector(
             `[data-row="${row}"][data-col="${col}"]`
         )
+        // const cell = document.querySelector(
+        //     `[data-row="${row}"][data-col="${col}"]`
+        // )
         placeMark(cell)
     }
 
@@ -89,11 +166,11 @@ let gameModule = (function gameModule() {
         if (_oTurn) {
             board.classList.remove(O.mark)
             board.classList.add(X.mark)
-            _currentTurn = X.mark
+            _currentTurn = X
         } else {
             board.classList.remove(X.mark)
             board.classList.add(O.mark)
-            _currentTurn = O.mark
+            _currentTurn = O
         }
         _oTurn = !_oTurn
     }
@@ -128,7 +205,9 @@ let gameModule = (function gameModule() {
 
     function displayWinner() {
         if (checkWin()) {
-            const content = document.createTextNode(`${_currentTurn}'s win!`)
+            const content = document.createTextNode(
+                `${_currentTurn.mark}'s win!`
+            )
             winnerText.appendChild(content)
             winningMessage.classList.add('show')
             return true
@@ -171,8 +250,15 @@ let gameModule = (function gameModule() {
         checkDraw,
         restart,
         getValidMoves,
+        minimax,
     }
 })()
 
 gameModule.start()
+// let origBoard = [
+//     ['X', 'X', ''],
+//     ['', 'O', ''],
+//     ['', '', ''],
+// ]
+// console.log(gameModule.minimax(origBoard, O))
 restartButton.addEventListener('click', gameModule.restart)
